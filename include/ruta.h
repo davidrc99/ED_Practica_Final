7d3357e30c69f93ca6f8c4457e88f45f581b370b
davidrc <davidromancastellano@gmail.com>
2020-12-24 18:34:51 +0100

    Cabecera clase Ruta

--- include/ruta.h
+++ include/ruta.h
@@ -9,8 +9,35 @@ class Ruta{
  private:
    list<Punto> puntos;
    string codigo;
+
+   void Copiar(const Ruta &r);
+   void Borrar(const Ruta &r);
  public:
-    Ruta(){}
+    Ruta();
+
+    Ruta(list<Punto> puntos_n, string codigo_n);
+
+    Ruta(const Ruta &r);
+
+    ~Ruta();
+
+    list<Punto> GetListaPuntos();
+
+    string GetCodigo();
+
+    bool estaPunto(const Punto & p);
+
+    void SetCodigo(const string valor);
+
+    void SetPuntos(const list<Punto> & puntos_n);
+
+    void InsertarPunto(const Punto & p);
+
+    Ruta & operator=(const Ruta &r);
+
+    friend ostream & operator<<(ostream & os, const Ruta & r){}
+
+    friend istream & operator>>(istream & is, Ruta & r){}
 
 
 };
