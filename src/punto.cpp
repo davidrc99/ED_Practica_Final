c462126288f2d5cb6590a3a6bda7a8a379bd0a53
davidrc <davidromancastellano@gmail.com>
2020-12-24 08:19:43 +0100

    Implementación de la Clase Punto (Incompleta)

--- src/punto.cpp
+++ src/punto.cpp
@@ -3,37 +3,41 @@
 using namespace std;
 
 Punto::Punto(){
-
+  lat = 0;
+  lng = 0;
+  descripcion = "";
 }
 
 Punto::Punto(double lat_n, double lng_n,string des_n){
-
+  lat = lat_n;
+  lng = lng_n;
+  descripcion = des_n;
 }
 
 Punto::Punto(const Punto &p){
-
+  copiar(p);  //Implementar
 }
 
 double Punto::GetLongitud(){
-
+  return lng;
 }
 
 double Punto::GetLatitud(){
-
+  return lat;
 }
 
 string Punto::GetDescripcion(){
-
+  return descripcion;
 }
 
 void Punto::SetLongitud(double valor){
-
+  lng = valor;
 }
 
 void Punto::SetLatitud(double valor){
-
+  lng = lat;
 }
 
 void Punto::SetDescripcion(string valor){
-
+  descripcion = valor;
 }
